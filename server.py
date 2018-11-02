f356790987574d7f9fa8fa95d5ec730f72a62773
Navjot <navjotts@gmail.com>
2018-11-02 03:55:37 -0700

    optimize memory requirements for deployment

--- server.py
+++ server.py
@@ -1,16 +1,23 @@
-import os
+import os, sys, shutil
 
 from starlette.applications import Starlette
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 import uvicorn
-from fastai import *
+from fastai.basic_train import *
 from fastai.vision import *
 
 from spectrogram import generate_spectrogram
 
 app = Starlette()
 app.mount('/static', StaticFiles(directory='static'))
+
+path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data')
+data_bunch = ImageDataBunch.single_from_classes(path, [0, 1],
+                                            tfms=get_transforms(do_flip=False, max_rotate=0., max_lighting=0., max_warp=0.),
+                                            size=224).normalize(imagenet_stats)
+learn = create_cnn(data_bunch, models.resnet34)
+learn.load('stage-2')
 
 @app.route('/')
 def index(request):
@@ -21,7 +28,6 @@ def index(request):
 async def analyze(request):
     data = await request.form()
     audio = await (data['file'].read())
-    path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data')
     uploadpath = os.path.join(path, 'uploads')
     try:
         os.makedirs(uploadpath)
@@ -29,11 +35,6 @@ async def analyze(request):
         f = open(filepath, 'wb')
         f.write(audio)
         f.close()
-        data_bunch = ImageDataBunch.single_from_classes(path, [0, 1],
-                                                    tfms=get_transforms(do_flip=False, max_rotate=0., max_lighting=0., max_warp=0.),
-                                                    size=224).normalize(imagenet_stats)
-        learn = create_cnn(data_bunch, models.resnet34)
-        learn.load('stage-2')
         img_filepath = generate_spectrogram(filepath)
         pred_class, _, _ = learn.predict(open_image(img_filepath))
     finally:
@@ -42,4 +43,5 @@ async def analyze(request):
     return JSONResponse({'crying': pred_class})
 
 if __name__ == '__main__':
-    uvicorn.run(app, host='0.0.0.0', port=5042)
+    if 'run' in sys.argv:
+        uvicorn.run(app, host='0.0.0.0', port=5042)
